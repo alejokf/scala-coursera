524181444e30fb88357b17d7374120e59ba8d09f
Alejandro Marulanda <alejokf@gmail.com>
2021-01-30 20:35:00 +0100

    Week 1 exercise definitions - add description

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -11,7 +11,15 @@ object Main {
   }
 
   /**
-   * Exercise 1
+   * Exercise 1: Pascal’s Triangle
+   *
+   * The numbers at the edge of the triangle are all 1, and each number inside the triangle is the sum of the two
+   * numbers above it. Write a function that computes the elements of Pascal’s triangle by means of a recursive process.
+   * 1
+   * 1 1
+   * 1 2 1
+   * 1 3 3 1
+   * 1 4 6 4 1
    */
   def pascal(c: Int, r: Int): Int = {
     if (c == 0 || c == r) 1
@@ -19,7 +27,10 @@ object Main {
   }
 
   /**
-   * Exercise 2
+   * Exercise 2: Parentheses Balancing
+   *
+   * Write a recursive function which verifies the balancing of parentheses in a string, which we represent as a
+   * List[Char] not a String.
    */
   def balance(chars: List[Char]): Boolean = {
     def balanceRec(count: Int, charsPar: List[Char]): Boolean = {
@@ -32,7 +43,11 @@ object Main {
   }
 
   /**
-   * Exercise 3
+   * Exercise 3: Counting Change
+   *
+   * Write a recursive function that counts how many different ways you can make change for an amount, given a list of
+   * coin denominations. For example, there are 3 ways to give change for 4 if you have coins with denomination 1 and 2:
+   * 1+1+1+1, 1+1+2, 2+2.
    */
   def countChange(money: Int, coins: List[Int]): Int = {
     def countChangeRec(money: Int, sortedCoins: List[Int]): Int = {
