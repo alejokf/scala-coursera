571c2d7265255a4862727371f16f7cc61d0ce6d2
Alejandro Marulanda <alejokf@gmail.com>
2021-01-30 13:18:01 +0100

    Week 1 exercise solutions

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -13,15 +13,34 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balanceRec(count: Int, charsPar: List[Char]): Boolean = {
+      if (count < 0) false
+      else if (charsPar.isEmpty) count == 0
+      else balanceRec(if (charsPar.head == '(') count + 1 else count - 1, charsPar.tail)
+    }
+
+    balanceRec(0, chars.filter(x => x == '(' || x == ')'))
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    def countChangeRec(money: Int, sortedCoins: List[Int]): Int = {
+      if (money == 0) 1
+      else if (money < 0 || sortedCoins.isEmpty) 0
+      else countChangeRec(money - sortedCoins.head, sortedCoins) + countChangeRec(money, sortedCoins.tail)
+    }
+
+    countChangeRec(money, coins.sorted(Ordering.Int.reverse))
+  }
 }
