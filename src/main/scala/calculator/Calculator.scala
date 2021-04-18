9818ac27da649b5df1993e2a0f854719f87df849
Alejandro Marulanda <alejokf@gmail.com>
2021-04-18 21:41:44 +0200

    Week 9 - Calculator exercise solution

--- src/main/scala/calculator/Calculator.scala
+++ src/main/scala/calculator/Calculator.scala
@@ -9,24 +9,48 @@ final case class Times(a: Expr, b: Expr) extends Expr
 final case class Divide(a: Expr, b: Expr) extends Expr
 
 object Calculator {
-  def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
+    namedExpressions map {
+      case (name, expr) => (name, Signal {
+        val exprVal = expr()
+        if (hasInfiniteCycle(name, exprVal, namedExpressions)) Double.NaN
+        else eval(exprVal, namedExpressions)
+      })
+    }
+  }
+
+  def hasInfiniteCycle(name: String, expr: Expr, references: Map[String, Signal[Expr]]): Boolean = {
+    expr match {
+      case Literal(_) => false
+      case Ref(refName) => {
+        if (name == refName) true
+        else references.get(refName) match {
+          case Some(refExpr) => hasInfiniteCycle(name, refExpr(), references)
+          case None => false
+        }
+      }
+      case Plus(a, b) => hasInfiniteCycle(name, a, references) || hasInfiniteCycle(name, b, references)
+      case Minus(a, b) => hasInfiniteCycle(name, a, references) || hasInfiniteCycle(name, b, references)
+      case Times(a, b) => hasInfiniteCycle(name, a, references) || hasInfiniteCycle(name, b, references)
+      case Divide(a, b) => hasInfiniteCycle(name, a, references) || hasInfiniteCycle(name, b, references)
+    }
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    expr match {
+      case Literal(v) => v
+      case Ref(name) => eval(getReferenceExpr(name, references), references)
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(b, references)
+      case Times(a, b) => eval(a, references) * eval(b, references)
+      case Divide(a, b) => eval(a, references) / eval(b, references)
+    }
   }
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
    */
-  private def getReferenceExpr(name: String,
-      references: Map[String, Signal[Expr]]) = {
-    references.get(name).fold[Expr] {
-      Literal(Double.NaN)
-    } { exprSignal =>
-      exprSignal()
-    }
+  private def getReferenceExpr(name: String, references: Map[String, Signal[Expr]]) = {
+    references.get(name).fold[Expr](Literal(Double.NaN))(exprSignal => exprSignal())
   }
 }
--- src/main/scala/calculator/Polynomial.scala
+++ src/main/scala/calculator/Polynomial.scala
@@ -3,11 +3,22 @@ package calculator
 object Polynomial {
   def computeDelta(a: Signal[Double], b: Signal[Double],
       c: Signal[Double]): Signal[Double] = {
-    ???
+    Signal {
+      Math.pow(b(), 2) - 4 * a() * c()
+    }
   }
 
   def computeSolutions(a: Signal[Double], b: Signal[Double],
       c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] = {
-    ???
+    Signal {
+      val deltaVal = delta()
+      if (deltaVal < 0) {
+        Set()
+      } else {
+        val aVal = a()
+        val bVal = b()
+        Set((-bVal + Math.sqrt(deltaVal)) / 2 * aVal, (-bVal - Math.sqrt(deltaVal)) / 2 * aVal)
+      }
+    }
   }
 }
